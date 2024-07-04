ff86cd0ea814953b8180258855be0d70592f258b
heqikun85 <heqikun@gmail.com>
2024-07-04 16:47:23 +0800

    modified:   .DS_Store 	modified:   Java note/.DS_Store 	modified:   "Java note/1. java\345\255\246\344\271\240\347\233\256\345\275\225.md" 	new file:   Java note/1720062960854.jpg 	new file:   Java note/1720064939709.jpg 	new file:   Java note/20180226113852869.png 	new file:   Java note/Snipaste_2022-09-05_21-36-50.png 	new file:   Java note/project/Test/.idea/compiler.xml 	modified:   Java note/project/Test/src/Demo1/Test1.java 	new file:   "Java note/\345\206\222\346\263\241.gif" 	new file:   "Java note/\345\270\270\347\224\250API.md" 	new file:   "Java note/\345\277\253\351\200\237\346\216\222\345\272\217.gif" 	modified:   "Java note/\346\213\274\345\233\276\346\270\270\346\210\217\351\241\271\347\233\256.md" 	new file:   "Java note/\346\216\222\345\272\217\347\256\227\346\263\225.md" 	new file:   "Java note/\346\217\222\345\205\245\346\216\222\345\272\217.gif" 	new file:   "Java note/\347\256\227\346\263\225.md" 	new file:   "Java note/\351\200\211\346\213\251\346\216\222\345\272\217.gif"

--- Java note/project/Test/src/Demo1/Test1.java
+++ Java note/project/Test/src/Demo1/Test1.java
@@ -1,26 +1,6 @@
 package Demo1;
 
-import javax.swing.*;
-import javax.swing.plaf.multi.MultiLookAndFeel;
-
 public class Test1 {
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame();
-        jFrame.setSize(603,680);
-        jFrame.setTitle("监听演示");
-        jFrame.setAlwaysOnTop(true);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setDefaultCloseOperation(3);
-        jFrame.setLayout(null);
-
-        //创建按钮对象
-        JButton jtb = new JButton("点我呀");
-        jtb.setBounds(0,0,100,50);
-        //创建按键监听，jtb组件对象，表示给哪个组件添加事件。
-        //addActionListener: 添加事件监听，参数（鼠标左键点击，空格）
-
-        jFrame.getContentPane().add(jtb);
-
-        jFrame.setVisible(true);
     }
 }
