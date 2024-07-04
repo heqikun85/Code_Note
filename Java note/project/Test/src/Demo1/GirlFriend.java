4e7c986a6de5ff711d55b529feb5942e38cfa1ed
heqikun85 <heqikun@gmail.com>
2024-07-04 17:49:16 +0800

    modified:   "Java note/1. java\345\255\246\344\271\240\347\233\256\345\275\225.md" 	modified:   Java note/project/Test/src/Demo1/GirlFriend.java 	modified:   Java note/project/Test/src/Demo1/Test1.java 	new file:   "Java note/\347\273\274\345\220\210\347\273\203\344\271\240.md"

--- Java note/project/Test/src/Demo1/GirlFriend.java
+++ Java note/project/Test/src/Demo1/GirlFriend.java
@@ -1,2 +1,5 @@
-package Demo1;public class GirlFriend {
+package Demo1;
+
+public class GirlFriend {
+    int onePair;
 }
--- Java note/project/Test/src/Demo1/Test1.java
+++ Java note/project/Test/src/Demo1/Test1.java
@@ -2,5 +2,18 @@ package Demo1;
 
 public class Test1 {
     public static void main(String[] args) {
+        int[] rabbit = new int[12];
+        rabbit[0] = 1;
+        rabbit[1] = 1;
+
+        System.out.println(getSum(12));
+    }
+
+    public static int getSum(int month) {
+        if (month == 1 || month == 2) {
+            return 1;
+        } else {
+            return getSum(month - 1) + getSum(month - 2);
+        }
     }
 }
