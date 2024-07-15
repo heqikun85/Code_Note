999b9741b07bdec39419921d38c3579ebbbace4c
heqikun85 <heqikun@gmail.com>
2024-07-15 21:37:57 +0800

    new file:   Java note/project/SpringExampleTest/.idea/vcs.xml 	modified:   Java note/project/SpringExampleTest/src/main/java/org/example/config/jdbcConfig.java 	modified:   "Spring/6.\347\254\254\344\270\211\346\226\271Bean\347\256\241\347\220\206.md"

--- Java note/project/SpringExampleTest/src/main/java/org/example/config/jdbcConfig.java
+++ Java note/project/SpringExampleTest/src/main/java/org/example/config/jdbcConfig.java
@@ -3,7 +3,7 @@ package org.example.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
 
 //@Configuration
