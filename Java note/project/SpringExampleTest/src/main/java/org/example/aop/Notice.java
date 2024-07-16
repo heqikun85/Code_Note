c89289345877c8dceee253169fd70f884b910419
heqikun85 <heqikun@gmail.com>
2024-07-16 13:54:48 +0800

    new file:   Java note/project/SpringExampleTest/src/main/java/org/example/AOP.java 	new file:   Java note/project/SpringExampleTest/src/main/java/org/example/aop/Notice.java 	modified:   Java note/project/SpringExampleTest/pom.xml 	modified:   Java note/project/SpringExampleTest/src/main/java/org/example/AOP.java 	modified:   Java note/project/SpringExampleTest/src/main/java/org/example/aop/Notice.java 	modified:   Java note/project/SpringExampleTest/src/main/java/org/example/dao/Impl/SpringDaoImpl.java 	modified:   Java note/project/SpringExampleTest/src/main/java/org/example/dao/SpringDao.java 	Spring/7.AOP.md 	Spring/image/1721095584247.jpg 	Spring/image/1721102314040.jpg

---
+++ Java note/project/SpringExampleTest/src/main/java/org/example/aop/Notice.java
@@ -0,0 +1,10 @@
+package org.example.aop;
+
+public class Nottice {
+    void method(){
+        //将共性功能单独提取出来，成为一个方法
+        //所谓共性功能，就是原设计中的功能，在增强版中仍然需要的功能
+        long startTime = System.currentTimeMillis();
+        System.out.println(startTime);
+    }
+}
