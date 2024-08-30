2b10c6810b1bc11f7c668ecb77b181e032ea6713
heqikun85 <heqikun@gmail.com>
2024-08-30 14:56:28 +0800

    modified:   Project/IdeaProject/SpringBootProject/.idea/workspace.xml 	modified:   Project/IdeaProject/myBatis-demo/pom.xml 	modified:   Project/IdeaProject/myBatis-demo/src/test/java/com/kxfevr/MyBatisTest.java 	new file:   Project/IdeaProject/myBatis-demo/target/classes/com/kxfevr/Main.class 	new file:   Project/IdeaProject/myBatis-demo/target/classes/com/kxfevr/entity/Brand.class 	new file:   Project/IdeaProject/myBatis-demo/target/classes/com/kxfevr/entity/User.class 	new file:   Project/IdeaProject/myBatis-demo/target/classes/com/kxfevr/mapper/BrandMapper.class 	new file:   Project/IdeaProject/myBatis-demo/target/classes/com/kxfevr/mapper/BrandMapper.xml 	new file:   Project/IdeaProject/myBatis-demo/target/classes/com/kxfevr/mapper/UserMapper.class 	new file:   Project/IdeaProject/myBatis-demo/target/classes/com/kxfevr/mapper/UserMapper.xml 	new file:   Project/IdeaProject/myBatis-demo/target/classes/logback.xml 	new file:   Project/IdeaProject/myBatis-demo/target/classes/mybatis-config.xml 	new file:   Project/IdeaProject/myBatis-demo/target/test-classes/com/kxfevr/MyBatisTest.class 	new file:   Project/IdeaProject/mybatisPlusDemo/.gitignore 	new file:   Project/IdeaProject/mybatisPlusDemo/.mvn/wrapper/maven-wrapper.properties 	new file:   Project/IdeaProject/mybatisPlusDemo/mvnw 	new file:   Project/IdeaProject/mybatisPlusDemo/mvnw.cmd 	new file:   Project/IdeaProject/mybatisPlusDemo/pom.xml 	new file:   Project/IdeaProject/mybatisPlusDemo/src/main/java/com/kxfevr/MybatisPlusDemoApplication.java 	modified:   Project/IdeaProject/mybatisPlusDemo/src/main/java/com/kxfevr/pojo/User.java 	modified:   Project/IdeaProject/mybatisPlusDemo/src/main/resources/com/kxfevr/mapper/UserMapper.xml 	modified:   Project/IdeaProject/mybatisPlusDemo/src/main/resources/logback.xml 	modified:   Project/IdeaProject/mybatisPlusDemo/src/main/resources/mybatis-config.xml 	new file:   Project/IdeaProject/mybatisPlusDemo/src/test/java/com/kxfevr/MybatisPlusDemoApplicationTests.java

--- Project/IdeaProject/myBatis-demo/src/test/java/com/kxfevr/MyBatisTest.java
+++ Project/IdeaProject/myBatis-demo/src/test/java/com/kxfevr/MyBatisTest.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 
 import java.io.IOException;
