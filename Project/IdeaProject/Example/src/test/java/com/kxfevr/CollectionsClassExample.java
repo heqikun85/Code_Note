422ae7b4576dd30ad89d272dffc46bc9a59fc870
heqikun85 <heqikun@gmail.com>
2024-09-24 17:33:32 +0800

    new file:   Project/IdeaProject/Example/.gitignore 	new file:   Project/IdeaProject/Example/.idea/.gitignore 	new file:   Project/IdeaProject/Example/pom.xml 	new file:   Project/IdeaProject/Example/src/main/java/com/kxfevr/App.java 	new file:   Project/IdeaProject/Example/src/test/java/com/kxfevr/AppTest.java 	renamed:    Project/IdeaProject/Test/src/Demo1/ArrayUtil.java -> Project/IdeaProject/Example/src/test/java/com/kxfevr/ArrayUtil.java 	renamed:    Project/IdeaProject/Test/src/Demo1/BinarySortTree.java -> Project/IdeaProject/Example/src/test/java/com/kxfevr/BinarySortTree.java 	renamed:    Project/IdeaProject/Test/src/Demo1/CollectionsClassExample.java -> Project/IdeaProject/Example/src/test/java/com/kxfevr/CollectionsClassExample.java 	renamed:    Project/IdeaProject/Test/src/Demo1/Stu.java -> Project/IdeaProject/Example/src/test/java/com/kxfevr/Stu.java 	renamed:    Project/IdeaProject/Test/src/Demo1/Student.java -> Project/IdeaProject/Example/src/test/java/com/kxfevr/Student.java 	renamed:    Project/IdeaProject/Test/src/Demo1/StudentTest.java -> Project/IdeaProject/Example/src/test/java/com/kxfevr/StudentTest.java 	renamed:    Project/IdeaProject/Test/src/Demo1/Test1.java -> Project/IdeaProject/Example/src/test/java/com/kxfevr/Test1.java 	modified:   "Java note/21. \346\226\271\346\263\225\345\274\225\347\224\250.md" 	modified:   Project/IdeaProject/EachModule/.idea/gradle.xml 	modified:   Project/IdeaProject/EachModule/.idea/workspace.xml 	modified:   Project/IdeaProject/Example/pom.xml 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/AppTest.java 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/ArrayUtil.java 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/BinarySortTree.java 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/CollectionsClassExample.java 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/Stu.java 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/Student.java 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/StudentTest.java 	modified:   Project/IdeaProject/Example/src/test/java/com/kxfevr/Test1.java 	deleted:    Project/IdeaProject/Test/Test.iml 	deleted:    Project/IdeaProject/changeName/.gitignore 	deleted:    Project/IdeaProject/changeName/.idea/.gitignore 	deleted:    Project/IdeaProject/changeName/.idea/encodings.xml 	deleted:    Project/IdeaProject/changeName/.idea/misc.xml 	deleted:    Project/IdeaProject/changeName/.idea/vcs.xml 	deleted:    Project/IdeaProject/changeName/pom.xml 	deleted:    Project/IdeaProject/changeName/src/main/java/com/kxfevr/Main.java 	"Java note/22. \345\274\202\345\270\270.md" 	Java note/23. file.md 	Project/IdeaProject/.idea/ 	Project/IdeaProject/Example/.idea/encodings.xml 	Project/IdeaProject/Example/.idea/misc.xml 	Project/IdeaProject/Example/.idea/vcs.xml

--- Project/IdeaProject/Example/src/test/java/com/kxfevr/CollectionsClassExample.java
+++ Project/IdeaProject/Example/src/test/java/com/kxfevr/CollectionsClassExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
-public class Test1 {
+public class CollectionsClassExample {
     public static void main(String[] args) {
         /*班级里有N个学生，学生属性：姓名，年龄，性别。
         实现随机点名器*/
